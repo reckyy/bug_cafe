d0dd90390e1253dc1454ed4eb6370a2e974eab93
reckyy <srreki98@gmail.com>
2023-07-22 09:28:27 +0900

    完了
    合計金額のエラーはhashのpriceを整数にすることで、
    メニュー間違いは（番号-1）とすることで、修正完了

--- cafe.rb
+++ cafe.rb
@@ -2,17 +2,17 @@
 require 'debug'
 
 DRINKS = [
-  { name: 'コーヒー', price: '300' },
-  { name: 'カフェラテ', price: '400' },
-  { name: 'チャイ', price: '460' },
-  { name: 'エスプレッソ', price: '340' },
-  { name: '緑茶', price: '450' }
+  { name: 'コーヒー', price: 300 },
+  { name: 'カフェラテ', price: 400 },
+  { name: 'チャイ', price: 460 },
+  { name: 'エスプレッソ', price: 340 },
+  { name: '緑茶', price: 450 }
 ].freeze
 
 FOODS = [
-  { name: 'チーズケーキ', price: '470' },
-  { name: 'アップルパイ', price: '520' },
-  { name: 'ホットサンド', price: '410' }
+  { name: 'チーズケーキ', price: 470 },
+  { name: 'アップルパイ', price: 520 },
+  { name: 'ホットサンド', price: 410 }
 ].freeze
 
 def take_order(menus)
@@ -31,6 +31,5 @@ order1 = take_order(DRINKS)
 puts 'フードメニューはいかがですか?'
 order2 = take_order(FOODS)
 
-total = DRINKS[order1][:price] + FOODS[order2][:price]
-binding.break
+total = DRINKS[(order1 - 1)][:price] + FOODS[(order2 - 1)][:price]
 puts "お会計は#{total}円になります。ありがとうございました！"
